a5332624fb9e1a17bcee2e233a7dd8c9d2f7c046
Jamez-1 <jamshaid.aziz121@gmail.com>
2023-04-06 04:06:16 +0500

    'Assignments'

---
+++ Assign001002.js
@@ -0,0 +1,18 @@
+//Assingmetn # 01//
+
+let userInput = +prompt(" Please ! Enter your age");
+if (userInput<=10){
+        console.log("You are Welcom!");
+}else{
+    console.log("Sorry! You are not allow here");
+};
+
+// Assignment # 02 //
+
+//print the TABLE of enter age of user via "for" loope//
+
+for (i=1; i<=10;i++){
+
+    console.log(userInput + " x " + i + " = " + userInput*i);
+
+};
---
+++ classOne.js
@@ -0,0 +1,11 @@
+// alert("Hello Java")
+
+let firstNum = 5;
+let secNum = 50;
+function add(){
+    return firstNum + secNum;
+};
+
+let sum = add()
+
+console.log(sum);
